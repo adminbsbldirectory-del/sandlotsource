550551e63c578dd25cca466998a3bacb8bad0a0b
adminbsbldirectory-del <admin.bsbldirectory@gmail.com>
2026-03-13 17:08:19 -0400

    Update CoachSubmitForm.jsx

--- src/components/CoachSubmitForm.jsx
+++ src/components/CoachSubmitForm.jsx
@@ -22,7 +22,7 @@ const REGIONS = {
   'Barrow','Banks','Cherokee','Clarke','Cobb','Dawson','DeKalb','Fannin',
   'Forsyth','Franklin','Fulton','Gilmer','Gordon','Gwinnett','Habersham',
   'Hall','Hart','Jackson','Lumpkin','Madison','Murray','Oconee','Pickens',
-  'Rabun','Stephens','Towns','Union','Walker','Walton','White','Whitfield',
+  'Rabun','Stephens','Towns','Union','Walker','Walton','White',
   ],
   'Middle Georgia': [
     'Baldwin','Bibb','Butts','Carroll','Catoosa','Chattooga','Clayton','Coweta',
