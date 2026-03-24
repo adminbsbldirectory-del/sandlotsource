d31cb3dc994781b9d8e5a3465fec1e14b9df995e
adminbsbldirectory-del <admin.bsbldirectory@gmail.com>
2026-03-24 07:57:07 -0400

    Update Facilities.jsx

--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -9,7 +9,7 @@ L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-})a
+})
 
 const HEADER_H = 75
 
